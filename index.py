94c1dea213b35eee0253e58b112e8716357c6fe1
Wiehahn <wiehahne@amazon.com>
2019-12-11 01:30:48 -0600

    s

--- index.py
+++ index.py
@@ -1,7 +1,16 @@
 from flask import Flask
+from flask import send_file
 app = Flask(__name__)
-@app.route("/winter")
-def hello():
-    return "Winter!"
+@app.route('/winter')
+def summer():
+    return "Winter App"
+@app.route('/winter/capetown')
+def amsterdam():
+    filename = 'amsterdam-480x300.jpg'
+    return send_file(filename, mimetype='image/gif')
+@app.route('/winter/keywest')
+def chicago():
+    filename = 'chicago-480x300.jpg'
+    return send_file(filename, mimetype='image/gif')
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int("80"), debug=True)
